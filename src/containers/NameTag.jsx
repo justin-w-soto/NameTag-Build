239046084e9c18efb699ef0b70a13532b969de72
Justin W Soto <soto@pdx.edu>
2021-11-05 15:12:51 -0700

    added container

--- src/containers/NameTag.jsx
+++ src/containers/NameTag.jsx
@@ -1,5 +1,25 @@
 import React, { Component } from 'react';
+import Controls from '../components/name-tag/Controls';
+import Display from '../components/name-tag/Display';
 
 export default class NameTag extends Component {
+state = {
+    greeting: '',
+    name: '',
+}
 
+render() {
+
+    return(
+
+        <>
+        
+        <Controls />
+        <Display />
+        
+        </>
+
+
+    )
+}
 }
